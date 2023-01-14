265d069b31de612f8fd33483d14ca94d3e39a2db
LetsCodeManh <manh.ng@icloud.com>
2023-01-14 20:13:43 +0100

    Install Prop and Apply for BookCard

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,5 +1,14 @@
+import { PropTypes } from "prop-types";
+
 const BookCard = ({ book, onBookClick }) => {
   return <div onClick={() => onBookClick(book)}>{book.title}</div>;
+};
+
+BookCard.propTypes = {
+  book: PropTypes.shape({
+    title: PropTypes.string,
+  }).isRequired,
+  onBookClick: PropTypes.func.isRequired,
 };
 
 export default BookCard;
